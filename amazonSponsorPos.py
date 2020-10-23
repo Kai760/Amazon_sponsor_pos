fb5b9a57735d88987fa895d1beab1e309f50ee64
kai_kurogi@negocia.jp <>
2020-10-23 20:32:49 +0900

    　fix bug

--- amazonSponsorPos.py
+++ amazonSponsorPos.py
@@ -4,18 +4,25 @@ import cv2
 import argparse
 
 
+def delete_left_sponsor(loc_sponsor):
+    pos = np.argmin(loc_sponsor[1])
+    loc_sponsor = np.delete(loc_sponsor, pos, axis=1)
+    return loc_sponsor
+
+
 def get_sponsor_pos(whole_img, target_img, sponsor_img='sponsor/sponsor.png'):
     # 画像の読み込み&この後の計算時間を考えてサイズを1/4へ
     img_rgb = cv2.imread(whole_img)
-    img_rgb = cv2.resize(img_rgb, (int(img_rgb.shape[1] * 0.5), int(img_rgb.shape[0] * 0.5)))
-    img_rgb = img_rgb[:, int(img_rgb.shape[1] * 0.24):]
+    img_rgb = cv2.resize(img_rgb, (int(img_rgb.shape[1] * 0.7), int(img_rgb.shape[0] * 0.7)))
+    # img_rgb = img_rgb[:, int(img_rgb.shape[1] * 0.24):]
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     template = cv2.imread(target_img, 0)
-    template = cv2.resize(template, (int(template.shape[1] * 0.5), int(template.shape[0] * 0.5)))
+    template = cv2.resize(template, (int(template.shape[1] * 0.7), int(template.shape[0] * 0.7)))
     template_base = template
     w, h = template.shape[::-1]
     sponsor = cv2.imread(sponsor_img, 0)
-    sponsor = cv2.resize(sponsor, (int(sponsor.shape[1] * 0.5), int(sponsor.shape[0] * 0.5)))
+    sponsor = cv2.resize(sponsor, (int(sponsor.shape[1] * 0.7), int(sponsor.shape[0] * 0.7)))
+    sponsor_base = sponsor
     threshold = 0
 
     # なぜかターゲット画像を取得した際に実際と違うサイズで取得される場合があるっぽいので、
@@ -39,17 +46,32 @@ def get_sponsor_pos(whole_img, target_img, sponsor_img='sponsor/sponsor.png'):
     pos_w, pos_h = loc[1][0], loc[0][0]
     cv2.rectangle(img_rgb, (pos_w, pos_h), (pos_w + w, pos_h + h), (0, 0, 255), 2)
 
+
     # スポンサープロダクトの位置を取得
-    res_sponsor = cv2.matchTemplate(img_gray, sponsor, cv2.TM_CCOEFF_NORMED)
-    loc_sponsor = np.where(res_sponsor >= 0.8)
+    threshold=0
+    count = 1
+    while threshold < 0.58:
+        res_sponsor = cv2.matchTemplate(img_gray, sponsor, cv2.TM_CCOEFF_NORMED)
+        threshold = res_sponsor.max() - 0.1
+        print(threshold)
+        coef = count * 0.01
+        if count % 2 == 0:
+            sponsor = cv2.resize(sponsor_base, (int(sponsor_base.shape[1] * (1 - coef)),
+                                                  int(sponsor_base.shape[0] * (1 - coef))))
+        else:
+            sponsor = cv2.resize(sponsor_base, (int(sponsor_base.shape[1] * (1 + coef)),
+                                                  int(sponsor_base.shape[0] * (1 + coef))))
+        count += 1
+    loc_sponsor = np.where(res_sponsor >= threshold)
     distance = 10000
     sponsor_pos = 100
+    delete_left_sponsor(loc_sponsor)
 
     # スポンサープロダクトの何番目かを計算
     for index, pt in enumerate(zip(*loc_sponsor[::-1])):
         cv2.rectangle(img_rgb, pt, (pt[0] + sponsor.shape[1], pt[1] + sponsor.shape[0]), (0, 0, 255), 2)
         temp_distance = abs(pt[0] - pos_w) + abs(pt[1] - (pos_h + h))
-        if temp_distance < distance:
+        if temp_distance < distance and abs(temp_distance-distance)>10:
             distance = temp_distance
             sponsor_pos = index + 1
 
@@ -59,8 +81,8 @@ def get_sponsor_pos(whole_img, target_img, sponsor_img='sponsor/sponsor.png'):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--whole_img', type=str, default='renzi.png')
-    parser.add_argument('--target_img', type=str, default='renzi_target.png')
+    parser.add_argument('--whole_img', type=str, default='display.png')
+    parser.add_argument('--target_img', type=str, default='target_display_2.png')
     opt = parser.parse_args()
     sponsor_pos = get_sponsor_pos(opt.whole_img, opt.target_img)
     print(f"今回の画像は{sponsor_pos}番目のスポンサープロダクトです。")
